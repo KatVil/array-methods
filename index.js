4d44fe53bfc2126ed06e3c3f6123cfc5b3f4e017
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-03-12 05:24:26 +0300

    try

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const filterByAge = (el) => {
-
+    return el.age < 50;
 };
 
 const filterByScore = (el) => {
-
+    return el.score > 75;
 };
 
 const filterByServiceRecord = (el) => {
-
+    return el.serviceRecord >= 5;
 };
 
 const sortByScore = (arr) => {
-
+    let newArr = arr.slice();
+    return newArr.sort(function (maxScore, minScore) {
+        return maxScore.score - minScore.score;
+    })
 };
 
 const getMeanAge = (arr) => {
-
+    let sum = 0;
+    for (let el of arr){
+        sum += el.age;
+    }
+    return sum/arr.length;
 };
 
 const getMeanAgeReduce = (arr) => {
-
+    let result = arr.reduce((acc, cur) => acc + cur.age, 0);
+    return result / arr.length;
 };
 
 const getNamesOnly = (arr) => {
-
+    return arr.name;
 };
 
 module.exports = {
